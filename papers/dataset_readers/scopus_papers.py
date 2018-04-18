66df64b97a21a894cf2e437f65d7ec25dc55e528
Sami Moustachir <moustachir.sami@gmail.com>
2018-04-18 15:28:57 +0200

    only abstract

--- papers/dataset_readers/scopus_papers.py
+++ papers/dataset_readers/scopus_papers.py
@@ -93,3 +93,79 @@ class ScopusDatasetReader(DatasetReader):
         token_indexers = TokenIndexer.dict_from_params(params.pop('token_indexers', {}))
         params.assert_empty(cls.__name__)
         return cls(lazy=lazy, tokenizer=tokenizer, token_indexers=token_indexers)
+
+@DatasetReader.register("scopusAbstract")
+class ScopusAbstractDatasetReader(DatasetReader):
+    """
+    Reads a JSON-lines file containing papers from the Semantic Scholar database, and creates a
+    dataset suitable for document classification using these papers.
+
+    Expected format for each input line: {"paperAbstract": "text", "title": "text", "venue": "text"}
+
+    The JSON could have other fields, too, but they are ignored.
+
+    The output of ``read`` is a list of ``Instance`` s with the fields:
+        abstract: ``TextField``
+        labels: List(``LabelField``)
+
+    where the ``label`` is derived from the venue of the paper.
+
+    Parameters
+    ----------
+    lazy : ``bool`` (optional, default=False)
+        Passed to ``DatasetReader``.  If this is ``True``, training will start sooner, but will
+        take longer per batch.  This also allows training with datasets that are too large to fit
+        in memory.
+    tokenizer : ``Tokenizer``, optional
+        Tokenizer to use to split the title and abstrct into words or other kinds of tokens.
+        Defaults to ``WordTokenizer()``.
+    token_indexers : ``Dict[str, TokenIndexer]``, optional
+        Indexers used to define input token representations. Defaults to ``{"tokens":
+        SingleIdTokenIndexer()}``.
+    """
+    def __init__(self,
+                 lazy: bool = True,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None) -> None:
+        super().__init__(lazy)
+        self._tokenizer = tokenizer or WordTokenizer()
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+
+    @overrides
+    def _read(self, file_path):
+        with open(file_path, 'r') as data_file:
+            reader = csv.reader(data_file)
+            next(reader, None)  # skip the headers
+            for row in tqdm.tqdm(reader):
+                abstract, _, _, title, *labels = row
+                yield self.text_to_instance(title, abstract, labels)
+
+    @overrides
+    def text_to_instance(self, abstract: str, labels: List[str] = None) -> Instance:  # type: ignore
+        # pylint: disable=arguments-differ
+        tokenized_abstract = self._tokenizer.tokenize(abstract)
+        title_field = TextField(tokenized_title, self._token_indexers)
+        abstract_field = TextField(tokenized_abstract, self._token_indexers)
+        fields = {'abstract': abstract_field}
+        if not labels:
+            labels = [0, 0, 0, 0]
+
+        health_sciences, life_sciences, physical_sciences, social_sciences = labels
+
+        # Because the labels are already 0 or 1, skip_indexing.
+        fields['labels'] = ListField([
+            LabelField(int(health_sciences),         skip_indexing=True),
+            LabelField(int(life_sciences),  skip_indexing=True),
+            LabelField(int(physical_sciences),       skip_indexing=True),
+            LabelField(int(social_sciences),        skip_indexing=True),
+        ])
+
+        return Instance(fields)
+
+    @classmethod
+    def from_params(cls, params: Params) -> 'ScopusDatasetReader':
+        lazy = params.pop('lazy', True)
+        tokenizer = Tokenizer.from_params(params.pop('tokenizer', {}))
+        token_indexers = TokenIndexer.dict_from_params(params.pop('token_indexers', {}))
+        params.assert_empty(cls.__name__)
+        return cls(lazy=lazy, tokenizer=tokenizer, token_indexers=token_indexers)
